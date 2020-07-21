97e54d04fb199640d7a6ca8e5dc51f0d472db47c
Memduh <memduhg@gmail.com>
2020-07-21 19:52:10 +0200

    iasdjad

--- projects/2019-2020-winter/memduh/scripts/eval/evaluate.sh
+++ projects/2019-2020-winter/memduh/scripts/eval/evaluate.sh
@@ -8,42 +8,69 @@ if [ ! -f scripts/multi-bleu-detok.perl ]; then
 wget https://raw.githubusercontent.com/EdinburghNLP/nematus/master/data/multi-bleu-detok.perl -P scripts
 fi
 
-for direction in `ls translations` 
+for model in `ls translations` 
 do
-	postproc=translations/$direction/postproc
-	mkdir -p $postproc
-	for file in `ls translations/$direction`
+	for direction in `ls translations/$model`
 	do
-		if [ ! -f $postproc/$file ]; then
-		cat translations/$direction/$file \
-			| sed 's/@@ //g'  \
-			| perl scripts/detokenizer.perl -u \
-			> $postproc/$file
-		fi
+		postproc=translations/$model/$direction/postproc
+		mkdir -p $postproc
+		for file in `ls translations/$model/$direction`
+		do
+			if [ ! -f $postproc/$file ]; then
+			cat translations/$model/$direction/$file \
+				| sed 's/@@ //g'  \
+				| perl scripts/detokenizer.perl -u \
+				> $postproc/$file
+			fi
+		done
 	done
 done
 
 mkdir -p results/default results/transformer
 if [ ! -f results/default/tr2en.txt ]; then
-	for file in `ls -v translations/tr2en/postproc`
+	for file in `ls -v translations/default/tr2en/postproc`
 	do
-		result=`perl scripts/multi-bleu-detok.perl data/dev.en < translations/tr2en/postproc/$file`
+		result=`perl scripts/multi-bleu-detok.perl data/dev.en < translations/default/tr2en/postproc/$file`
 		echo $file - $result >> results/default/tr2en.txt
 	done
 fi
 
 if [ ! -f results/default/tr2en_analyzed.txt ]; then
-	for file in `ls -v translations/tr2en_analyzed/postproc`
+	for file in `ls -v translations/default/tr2en_analyzed/postproc`
 	do
-		result=`perl scripts/multi-bleu-detok.perl data/dev.en < translations/tr2en_analyzed/postproc/$file`
+		result=`perl scripts/multi-bleu-detok.perl data/dev.en < translations/default/tr2en_analyzed/postproc/$file`
 		echo $file - $result >> results/default/tr2en_analyzed.txt
 	done
 fi
 
 if [ ! -f results/default/en2tr.txt ]; then
-	for file in `ls -v translations/en2tr/postproc`
+	for file in `ls -v translations/default/en2tr/postproc`
 	do
-		result=`perl scripts/multi-bleu-detok.perl data/dev.tr < translations/en2tr/postproc/$file`
+		result=`perl scripts/multi-bleu-detok.perl data/dev.tr < translations/default/en2tr/postproc/$file`
 		echo $file - $result >> results/default/en2tr.txt
+	done
+fi
+
+if [ ! -f results/transformer/tr2en.txt ]; then
+	for file in `ls -v translations/transformer/tr2en/postproc`
+	do
+		result=`perl scripts/multi-bleu-detok.perl data/dev.en < translations/transformer/tr2en/postproc/$file`
+		echo $file - $result >> results/transformer/tr2en.txt
+	done
+fi
+
+if [ ! -f results/transformer/tr2en_analyzed.txt ]; then
+	for file in `ls -v translations/transformer/tr2en_analyzed/postproc`
+	do
+		result=`perl scripts/multi-bleu-detok.perl data/dev.en < translations/transformer/tr2en_analyzed/postproc/$file`
+		echo $file - $result >> results/transformer/tr2en_analyzed.txt
+	done
+fi
+
+if [ ! -f results/transformer/en2tr.txt ]; then
+	for file in `ls -v translations/transformer/en2tr/postproc`
+	do
+		result=`perl scripts/multi-bleu-detok.perl data/dev.tr < translations/transformer/en2tr/postproc/$file`
+		echo $file - $result >> results/transformer/en2tr.txt
 	done
 fi
